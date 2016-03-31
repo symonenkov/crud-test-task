90a3794044cdb3d2a6542231a53136a4065bd425
symonenkov <symonenkov.artem@hotmail.com>
2016-03-31 15:30:20 +0300

    Added files via upload

---
+++ front-end/common/userResource.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    "use strict";
+
+    angular
+		.module("common.services")
+        .factory("userResource",
+                 ["$resource",
+                  "appSettings",
+                    userResource])
+
+    function userResource($resource, appSettings) {
+
+        return {
+            List: $resource(appSettings.serverPath + "/api/t_user/", {headers: { 'If-None-Match': '' }}),
+            Post: $resource(appSettings.serverPath + "/api/t_user/",{'isArray': false} ,{headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}}),
+            Delete: $resource(appSettings.serverPath + "/api/t_user/:id"),
+            Update: $resource(appSettings.serverPath + "/api/t_user/:id", null, 
+            { 'update': { method:'PUT' }})
+        }
+    }
+
+}());
